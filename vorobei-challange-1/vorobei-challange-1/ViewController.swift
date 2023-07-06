e27b84d7552aeb8eda7e8ce8444b6a2d57a87758
Alexey Voronov <voronoff2803@gmail.com>
2023-07-06 20:34:05 +0300

    challange 2 done

--- vorobei-challange-1/vorobei-challange-1/ViewController.swift
+++ vorobei-challange-1/vorobei-challange-1/ViewController.swift
@@ -7,60 +7,139 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    let customView = CustomView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+class ViewController : UIViewController {
+    let button1 = CustomButton(type: .custom)
+    let button2 = CustomButton(type: .custom)
+    let button3 = CustomButton(type: .custom)
+    
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.addSubview(customView)
-        setupUI()
+        self.view = view
     }
     
-    func setupUI() {
-        customView.translatesAutoresizingMaskIntoConstraints = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        let img1 = UIImage(systemName: "arrowshape.turn.up.right.circle.fill")
+        
+        UIView.performWithoutAnimation {
+            self.button1.setTitle("First Button", for: .normal)
+            self.button1.setImage(img1, for: .normal)
+            
+            self.button2.setTitle("Second Medium Button", for: .normal)
+            self.button2.setImage(img1, for: .normal)
+            
+            self.button3.setTitle("Third", for: .normal)
+            self.button3.setImage(img1, for: .normal)
+            self.button3.addTarget(self, action: #selector(self.action), for: .touchUpInside)
+        }
+        
+        
+        
+        let stackView = UIStackView(arrangedSubviews: [button1, button2, button3])
+        stackView.spacing = 10
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(stackView)
+//
         NSLayoutConstraint.activate([
-            customView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            customView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100),
-            customView.heightAnchor.constraint(equalToConstant: 100),
-            customView.widthAnchor.constraint(equalToConstant: 100)
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
+    }
+    
+    @objc func action() {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .white
+        self.present(vc, animated: true)
     }
 }
 
 
-class CustomView: UIView {
-    let gradientLayer = {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [UIColor.blue.cgColor, UIColor.red.cgColor]
-        gradientLayer.startPoint = CGPoint.zero
-        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
-        gradientLayer.locations = [0.0,1.0]
-        gradientLayer.cornerRadius = 10
-        return gradientLayer
-    }()
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        gradientLayer.frame = self.bounds
+
+class CustomButton: UIButton {
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted == oldValue { return }
+            UIView.animate(withDuration: 0.2, delay: 0.0, options: [.beginFromCurrentState]) {
+                self.transform = self.isHighlighted ? .init(scaleX: 0.9, y: 0.9) : .identity
+            }
+        }
     }
     
+    
+    override var isEnabled: Bool {
+        didSet {
+            print(isEnabled)
+        }
+    }
+    
+    
+    let spacing: CGFloat = 8
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        self.layer.addSublayer(gradientLayer)
-        self.setupShadow()
+        commonInit()
     }
-    
-    func setupShadow() {
-        self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowOpacity = 1
-        self.layer.shadowOffset = .zero
-        self.layer.shadowRadius = 12
-    }
-    
+
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        backgroundColor = tintColor
+        self.tintAdjustmentMode = .normal
+        self.layer.cornerRadius = 10
+        self.imageView?.tintColor = self.titleColor(for: .normal)
+        self.adjustsImageWhenHighlighted = false
+        self.translatesAutoresizingMaskIntoConstraints = false
+        tintAdjustmentMode = .automatic
+        
+        titleLabel?.font = UIFont.systemFont(ofSize: 17)
+        
+        layoutIfNeeded()
+    }
+
+    override var intrinsicContentSize: CGSize {
+        var size = super.intrinsicContentSize
+        size.width += 14 * 2 + spacing
+        size.height += 10 * 2
+        
+        return size
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if let imageView = imageView, let titleLabel = titleLabel {
+            let titleWidth = titleLabel.frame.width
+            let imageWidth = imageView.frame.width
+
+            let totalWidth = titleWidth + spacing + imageWidth
+
+            titleLabel.frame.origin.x = (bounds.width - totalWidth) / 2
+            imageView.frame.origin.x = titleLabel.frame.maxX + spacing
+        }
+    }
+    
+    override func tintColorDidChange() {
+        UIView.animate(withDuration: 0.2) {
+                if self.tintColor == .systemBlue {
+                    self.backgroundColor = self.tintColor
+                    self.titleLabel?.textColor = .white
+                    self.imageView?.tintColor = .white
+                } else {
+                    self.backgroundColor = .systemGray2
+                    self.titleLabel?.textColor = .systemGray3
+                    self.imageView?.tintColor = .systemGray3
+                }
+            }
+//        }
+        
     }
 }
